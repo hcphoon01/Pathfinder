4aacfa020f48a9b63817ea343326373ad5a81876
Harry Cameron <hcphoon01@gmail.com>
2019-03-14 21:07:58 +0000

    some updates

--- draw.py
+++ draw.py
@@ -1,20 +1,53 @@
-<<<<<<< HEAD
 import pygame
 
+# Define colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+(g_width, g_height) = (20, 20)
+margin = 1
+
 class Draw:
-	def addWall(pos):
+	def Wall(self, pos, win, status):
 		column = pos[0] // (g_width + margin)
 		row = pos[1] // (g_height + margin)
 		rect = ((margin + g_width) * column + margin, (margin + g_height) * row + margin, g_width, g_height)
-		colour = BLACK
-=======
-import pygame
+		if status == "add":
+			colour = BLACK
+		elif status == "remove":
+			colour = WHITE
+		pygame.draw.rect(win, colour, rect)
+		coords = []
+		coords.extend([column, row])
+		return coords
 
-class Draw:
-	def addWall(pos):
+	def Start(self, pos, win, status):
 		column = pos[0] // (g_width + margin)
 		row = pos[1] // (g_height + margin)
 		rect = ((margin + g_width) * column + margin, (margin + g_height) * row + margin, g_width, g_height)
-		colour = BLACK
->>>>>>> 9ed9dde44e46dc9d7ec244a384416bf374c3de10
-		return pygame.draw.rect(win, colour, rect)
+		if status == "add":
+			colour = GREEN
+		elif status == "remove":
+			colour = WHITE
+		pygame.draw.rect(win, colour, rect)
+		coords = []
+		coords.extend([column, row])
+		return coords
+
+	def End(self, pos, win, status):
+		column = pos[0] // (g_width + margin)
+		row = pos[1] // (g_height + margin)
+		rect = ((margin + g_width) * column + margin, (margin + g_height) * row + margin, g_width, g_height)
+		if status == "add":
+			colour = RED
+		elif status == "remove":
+			colour = WHITE
+		coords = []
+		coords.extend([column, row])
+		return pygame.draw.rect(win, colour, rect), coords 
+
+
+		
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 from draw import Draw
 
@@ -17,6 +16,10 @@ M_RIGHT = 3
 (width, height) = (800, 800)
 (g_width, g_height) = (20, 20)
 margin = 1
+
+wall_list = []
+start = 0
+end = 0
 
 grid = []
 for row in range((height // g_height)-2):
@@ -43,78 +46,65 @@ clock = pygame.time.Clock()
 
 while not done:
 	for event in pygame.event.get():
+		keys = pygame.key.get_pressed()
+		mouse = pygame.mouse.get_pressed()
 		if event.type == pygame.QUIT:
 			done = True
-		elif event.type == pygame.MOUSEBUTTONUP and event.button == M_LEFT:
-			draw.addWall(pygame.mouse.get_pos())
-		elif event.type == pygame.MOUSEBUTTONDOWN and event.button == M_RIGHT:
-			pos = pygame.mouse.get_pos()
-			column = pos[0] // (g_width + margin)
-			row = pos[1] // (g_height + margin)
-			grid[row][column] = 0
+
+		if mouse[0]:
+			if all(a == 0 for a in keys):
+				pos = pygame.mouse.get_pos()
+				if ([pos[0] // (g_width + margin), pos[1] // (g_height + margin)]) == start or end:
+					continue;
+				coords = Draw().Wall(pos, win, "add")
+				if coords not in wall_list:
+					wall_list.append(coords)
+			if keys[pygame.K_s] and start == 0:
+				pos = pygame.mouse.get_pos()
+				coords = Draw().Start(pos, win, "add")
+				start = coords
+			if keys[pygame.K_e] and end == 0:
+				pos = pygame.mouse.get_pos()
+				coords = Draw().End(pos, win, "add")
+				end = coords
+		
+		if mouse[2]:
+			if all(a == 0 for a in keys):
+				pos = pygame.mouse.get_pos()
+				if ([pos[0] // (g_width + margin), pos[1] // (g_height + margin)]) in wall_list:
+					coords = Draw().Wall(pos, win, "remove")
+				elif ([pos[0] // (g_width + margin), pos[1] // (g_height + margin)]) == start:
+					Draw().Start(pos, win, "remove")
+					start = 0
+				elif ([pos[0] // (g_width + margin), pos[1] // (g_height + margin)]) == end:
+					Draw().End(pos, win, "remove")
+					end = 0
+			if keys[pygame.K_s] and start != 0:
+				pos = pygame.mouse.get_pos()
+				Draw().Start(pos, win, "remove")
+				start = 0
+			if keys[pygame.K_e] and end != 0:
+				pos = pygame.mouse.get_pos()
+				Draw().End(pos, win, "remove")
+				end = 0
+
+		if keys[pygame.K_c]:
+			for row in range((height // g_height)-2):
+				for column in range((width // g_width)-2):
+					colour = WHITE
+					rect = ((margin + g_width) * column + margin, (margin + g_height) * row + margin, g_width, g_height)
+					pygame.draw.rect(win, colour, rect)
+					del wall_list[:]
+		if keys[pygame.K_SPACE]:
+			print(wall_list)
+
+		if keys[pygame.K_o]:
+			menu = pygame.Surface((200, 200))
+			menu.fill((50, 50, 50))
+			win.blit(menu, (width, height))
+			pygame.display.flip
 
 	clock.tick(60)
-
 	pygame.display.flip()
 
-=======
-import pygame
-from draw import Draw
-
-# Define colours
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-# Define key presses
-	# Mouse keys
-M_LEFT = 1
-M_RIGHT = 3
-
-(width, height) = (800, 800)
-(g_width, g_height) = (20, 20)
-margin = 1
-
-grid = []
-for row in range((height // g_height)-2):
-	grid.append([])
-	for column in range((width // g_width)-2):
-		grid[row].append(0)
-
-pygame.init()
-win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("A* Pathfinding")
-
-win.fill(BLACK)
-
-for row in range((height // g_height)-2):
-		for column in range((width // g_width)-2):
-			colour = WHITE
-			rect = ((margin + g_width) * column + margin, (margin + g_height) * row + margin, g_width, g_height)
-			pygame.draw.rect(win, colour, rect)
-
-
-done = False
-
-clock = pygame.time.Clock()
-
-while not done:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			done = True
-		elif event.type == pygame.MOUSEBUTTONUP and event.button == M_LEFT:
-			draw.addWall(pygame.mouse.get_pos())
-		elif event.type == pygame.MOUSEBUTTONDOWN and event.button == M_RIGHT:
-			pos = pygame.mouse.get_pos()
-			column = pos[0] // (g_width + margin)
-			row = pos[1] // (g_height + margin)
-			grid[row][column] = 0
-
-	clock.tick(60)
-
-	pygame.display.flip()
-
->>>>>>> 9ed9dde44e46dc9d7ec244a384416bf374c3de10
 pygame.quit()
